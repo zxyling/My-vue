da4535b1414d16955d6490d7373d58d93eeb7ae5
zxyling <zxyling@qq.com>
2016-07-25 02:09:06 +0800

    0725

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">
+        {{msg}}
+    </div>
+</template>
+<style>
+    .header{
+        background-color:#ff0000;
+    }
+</style>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        }
+    }
+</script>
---
+++ src/pages/index/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangxiaoyang on 2016/7/25.
+ */
+import app from '../../components/header.vue';
+//var app = require('../../components/header.vue');
+new Vue({
+    el:'body',
+    components:{
+        app: app
+    }
+});
