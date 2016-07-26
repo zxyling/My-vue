7b65c7a02620bc95019bb5a1e9eb7c4c0325a369
zxyling <zxyling@qq.com>
2016-07-27 02:04:01 +0800

    0727

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,20 +1,17 @@
 <template>
-    <div class="header">
-        {{msg}}
+    <div class="box">
+        {{message}}
     </div>
 </template>
 <style>
-    .header{
+    .box{
         background-color:#ff0000;
     }
 </style>
 <script>
     export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
+        props:{
+            message:String
         }
     }
-    alert(1);
 </script>
